3d646e9f52bd68d6bc137fc41ad3f769b1aad95f
marctaylor700 <marctaylor700@gmail.com>
2021-03-31 16:02:11 -0700

    add parallel configuration updates

--- conf/parallel.conf.js
+++ conf/parallel.conf.js
@@ -1,5 +1,21 @@
+const { version } = require("chromedriver");
+
+// src folders can change depending on the tests you'd like to run. target the correct one or you'll have a bad time.
 nightwatch_config = {
-  src_folders : [ "tests/single" ],
+  src_folders : [ "tests/patient" ],
+
+  custom_commands_path: ["./commands"],
+  page_objects_path: ["pages"],
+
+  //global variables used in tests
+  globals: {
+    env: "release",
+    handle: "omega",
+    email:"taylor+o14@evisit.com",
+    password:"Patient123!",
+    providerEmail: "taylor+provider@evisit.com",
+    providerPassword: "Provider123!"
+  },
 
   selenium : {
     "start_process" : false,
@@ -7,26 +23,35 @@ nightwatch_config = {
     "port" : 80
   },
 
+  //Insert capabilities you'd like to use globally across browsers
   common_capabilities: {
     'build': 'nightwatch-browserstack',
     'browserstack.user': process.env.BROWSERSTACK_USERNAME || 'nick34',
     'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY || 'SBukT13jtljJiBWoBpHw',
-    'browserstack.debug': true
+    'browserstack.debug': true,
+    'browserstack.timezone' : "Phoenix",
+    'browserstack.geoLocation' : "BR",
   },
 
   test_settings: {
     default: {},
+
     chrome: {
       desiredCapabilities: {
-        browser: "chrome"
+        browser: "chrome",
+        version: "89.0",
+        os: "Windows",
+        os_version: "10"
         //resolution: '1024x768'
         
       }
     },
     firefox: {
       desiredCapabilities: {
-        browser: "firefox"
-        //resolution: '1024x768'
+        browser: "firefox",
+        version: "87.0",
+        os: "OS X",
+        os_version: "Big Sur"
       }
     },
     safari: {
@@ -40,7 +65,10 @@ nightwatch_config = {
       }
     }
   }
-};
+}
+
+
+
 
 // Code to support common capabilites
 for(var i in nightwatch_config.test_settings){
